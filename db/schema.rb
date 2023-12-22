61e4782e14749dd3c1b00c6da87683bbffd81312
Eduardo M Mayor <eduardomanuelmayor@gmail.com>
2023-12-22 07:50:37 -0500

    skill model

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2023_12_21_130713) do
+ActiveRecord::Schema[7.1].define(version: 2023_12_22_123844) do
   create_table "blogs", force: :cascade do |t|
     t.string "title"
     t.text "body"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "skills", force: :cascade do |t|
+    t.string "title"
+    t.integer "percent_utilized"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
